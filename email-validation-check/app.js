220c7f27d5df89d179f1810bf5b43acfb64605e2
beratberkayg <berat.1675@gmail.com>
2023-09-07 14:17:53 +0300

    create app.js

---
+++ email-validation-check/app.js
@@ -0,0 +1,25 @@
+function validition() {
+  const form = document.getElementById("form");
+  const email = document.getElementById("email").value;
+  const text = document.getElementById("text");
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "Your Email Address in Valid";
+    text.style.color = "#00ff00";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please Enter Valid Email Address";
+    text.style.color = "#ff0000";
+  }
+
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+    text.style.color = "#00ff00";
+  }
+}
