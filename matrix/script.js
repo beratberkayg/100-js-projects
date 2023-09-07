115b8f9fed802ef294ec42fb128ecc0da1775593
beratberkayg <berat.1675@gmail.com>
2023-09-07 14:34:00 +0300

    create script.js
    I like this project...

---
+++ matrix/script.js
@@ -0,0 +1,34 @@
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext("2d");
+let W = window.innerWidth;
+let H = window.innerHeight;
+
+canvas.width = W;
+canvas.height = H;
+
+let fontSize = 16;
+let columns = Math.floor(W / fontSize);
+let drops = [];
+for (let i = 0; i < columns; i++) {
+  drops.push(0);
+}
+
+let str = "BERAT BERKAY GÖKDEMİR";
+function draw() {
+  context.fillStyle = "rgba(0,0,0,0.05)";
+  context.fillRect(0, 0, W, H);
+  context.fontSize = "700" + fontSize + "px";
+  context.fillStyle = "#00cc33";
+  for (let i = 0; i < columns; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    let x = i * fontSize;
+    let y = drops[i] * fontSize;
+    context.fillText(str[index], x, y);
+    if (y >= canvas.height && Math.random() > 0.99) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  }
+}
+draw();
+setInterval(draw, 35);
